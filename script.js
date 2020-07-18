89948339a132d725efaea98655774b85cf23bdc7
gbolahan balogun <gbolahanbalo2018@gmail.com>
2020-07-19 00:53:11 +0100

    add animation on scroll

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let navbar = document.getElementById('nav-menu-btn');
 let menu = document.querySelector('.menu');
 let container = document.querySelector('.container');
+let menuicon = document.querySelector('.menu-icon');
+let closeicon = document.querySelector('.close-icon');
 navbar.addEventListener('click', (e) => {
     menu.style.display = 'block';
-    newnav = navbar.setAttribute('src', "img/icon-close.svg");
+    menuicon.style.display = 'none';
+    closeicon.style.display = 'block';
 });
+closeicon.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuicon.style.display = 'block';
+    closeicon.style.display = 'none';
+});
+
+
 container.addEventListener('click', () => {
     menu.style.display = 'none';
-    newnav = navbar.setAttribute('src', "img/icon-hamburger.svg");
+    navbar.setAttribute('src', "img/icon-hamburger.svg");
 });
